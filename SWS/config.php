e979e56f7c664291f49ba96f6f2d249e5dcd992f
Ángel Aguilar <mgordillo0@ucol.mx>
2019-05-06 11:07:25 -0500

    Update 2.0

--- SWS/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'angel');
-    define('DB_PASSWORD', 'angel1316');
-    define('DB_DATABASE', 'angel');
-    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
