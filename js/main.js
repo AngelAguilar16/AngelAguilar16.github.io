f16f9fbb6386d8c0abc83f8fe0c5595800b41b72
Ángel Aguilar <mgordillo0@ucol.mx>
2020-01-29 23:16:36 -0600

    Update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 // Wrap every letter in a span
-var textWrapper = document.querySelector('.ml12');
+var textWrapper = document.querySelector('.ml13');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+var textWrapper_2 = document.querySelector('.ml14');
+textWrapper_2.innerHTML = textWrapper_2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
   .add({
-    targets: '.ml12 .letter',
+    targets: '.ml13 .letter',
     translateX: [40,0],
     translateZ: 0,
     opacity: [0,1],
@@ -12,10 +14,25 @@ anime.timeline({loop: true})
     duration: 1200,
     delay: (el, i) => 500 + 30 * i
   }).add({
-    targets: '.ml12 .letter',
+    targets: '.ml13 .letter',
     translateX: [0,-30],
     opacity: [1,0],
     easing: "easeInExpo",
     duration: 1100,
     delay: (el, i) => 100 + 30 * i
-});
+  }).add({
+    targets: '.ml14 .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.ml14 .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
